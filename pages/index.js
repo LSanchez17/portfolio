de2199b1f72f759717fe575c9cd0999837d9be1c
LSanchez17 <galaxyplayeremail@gmail.com>
2021-05-10 20:41:43 -0400

    working on landing page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import dynamic from 'next/dynamic';
+import { useEffect } from 'react';
 
 const Particles = dynamic(
   //dynamic sideloads the npm packages that require window access
@@ -14,51 +15,78 @@ const Particles = dynamic(
 )
 
 export default function Home() {
+  let show = false;
+
+  // useEffect(() => {
+  //   const FadeIn = () => {
+  //     window.setTimeout(() => {
+  //       show = !show;
+  //     }, 1000);
+  //   }
+
+  //   FadeIn();
+  // }, [])
+
+  let homePage = (<div className={styles.container}>
+    <Head>
+      <title>Luis-Software Engineer</title>
+      <meta name="description" content="Luis, Software Engineer" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main className={styles.main}>
+      <Particles color='000000' type='cobweb' bg={true} />
+      <h3 className={styles.title}>
+      Welcome to my humble abode
+      </h3>
+
+      <div className={styles.grid}>
+        <Link href="/projects" className={styles.card}>
+          <div className={styles.card}>
+            <h2><a>Projects &rarr;</a></h2>
+            <p>View an evergrowing list of projects I've completed and work on</p>  
+          </div>
+        </Link>
+
+        <Link href="/contact" className={styles.card}>
+          <div className={styles.card}>
+            <h2><a>Contact &rarr;</a></h2>
+            <p>Want to initiate a dialogue, have questions, comments, compliments? </p>
+          </div>
+        </Link>
+
+        <Link href="/about" className={styles.card}>
+          <div className={styles.card}>
+            <h2><a>About &rarr;</a></h2>
+            <p>Discover more about your next potential hire</p>
+          </div>
+        </Link>
+        
+
+      </div>
+    </main>
+
+    <footer className={styles.footer}>
+    <p>icons provided from <span>https://www.flaticon.com/</span></p>
+    </footer>
+  </div>)
+  let loadingDock = (
+    <div className='border rounded-light-blue-800 shadow'>
+  <div class="animate-pulse flex space-x-4">
+    <div class="rounded-full bg-light-blue-400 h-12 w-12"></div>
+    <div class="flex-1 space-y-4 py-1">
+      <div class="h-4 bg-light-blue-400 rounded w-3/4"></div>
+      <div class="space-y-2">
+        <div class="h-4 bg-light-blue-400 rounded"></div>
+        <div class="h-4 bg-light-blue-400 rounded w-5/6"></div>
+      </div>
+    </div>
+  </div>
+  </div>)
+
   return (
-    <div className='w-screen h-screen bg-gradient-to-b from-purple-500 via-blue-500 to-green-400 bg-opacity-50'>
-      <div className={styles.container}>
-      <Head>
-        <title>Luis-Software Engineer</title>
-        <meta name="description" content="Luis, Software Engineer" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <Particles color='000000' type='cobweb' bg={true} />
-        <h3 className={styles.title}>
-        Welcome to my humble abode
-        </h3>
-
-        <div className={styles.grid}>
-          <Link href="/projects" className={styles.card}>
-            <div className={styles.card}>
-              <h2><a>Projects &rarr;</a></h2>
-              <p>View an evergrowing list of projects I've completed and work on</p>  
-            </div>
-          </Link>
-
-          <Link href="/contact" className={styles.card}>
-            <div className={styles.card}>
-              <h2><a>Contact &rarr;</a></h2>
-              <p>Want to initiate a dialogue, have questions, comments, compliments? </p>
-            </div>
-          </Link>
-
-          <Link href="/about" className={styles.card}>
-            <div className={styles.card}>
-              <h2><a>About &rarr;</a></h2>
-              <p>Discover more about your next potential hire</p>
-            </div>
-          </Link>
-          
-
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-      <p>icons provided from <span>https://www.flaticon.com/</span></p>
-      </footer>
-    </div>
-    </div>
+    // <div className='w-screen h-screen bg-gradient-to-b from-purple-500 via-blue-500 to-green-400 bg-opacity-50'>
+   <>     {show ? homePage : loadingDock} </>
+    // </div>
   )
 }
