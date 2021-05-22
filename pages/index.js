67a2898fc812dc23fb65b29a074a1df3b5812756
LSanchez17 <galaxyplayeremail@gmail.com>
2021-05-21 23:05:58 -0400

    restructuring homepage

--- pages/index.js
+++ pages/index.js
@@ -70,23 +70,11 @@ export default function Home() {
     <p>icons provided from <span>https://www.flaticon.com/</span></p>
     </footer>
   </div>)
-  let loadingDock = (
-    <div className='border rounded-light-blue-800 shadow'>
-  <div class="animate-pulse flex space-x-4">
-    <div class="rounded-full bg-light-blue-400 h-12 w-12"></div>
-    <div class="flex-1 space-y-4 py-1">
-      <div class="h-4 bg-light-blue-400 rounded w-3/4"></div>
-      <div class="space-y-2">
-        <div class="h-4 bg-light-blue-400 rounded"></div>
-        <div class="h-4 bg-light-blue-400 rounded w-5/6"></div>
-      </div>
-    </div>
-  </div>
-  </div>)
+
 
   return (
-    // <div className='w-screen h-screen bg-gradient-to-b from-purple-500 via-blue-500 to-green-400 bg-opacity-50'>
-   <>     {show ? homePage : loadingDock} </>
-    // </div>
+    <div className='w-screen h-screen bg-gradient-to-b from-purple-500 via-blue-500 to-green-400 bg-opacity-50'>
+      {homePage}
+    </div>
   )
 }
